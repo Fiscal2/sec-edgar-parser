75d9b43c734f63008bcf0231c6b3c35b9b820a00
Zac Goldberg <zacharyross3@gmail.com>
2025-07-11 22:45:22 -0400

    main.py test

--- edgar/requests_wrapper.py
+++ edgar/requests_wrapper.py
@@ -1,8 +1,14 @@
 import requests
 
+headers = {
+            "User-Agent": "Zac G AdminContact@samplecompanydomain.com",  # Use real contact email
+            "Accept-Encoding": "gzip, deflate",
+            "Host": "www.sec.gov"
+        }
+
 class GetRequest:
     def __init__(self, url):
-        response = requests.get(url)
+        response = requests.get(url, headers=headers)
         response.encoding = 'utf-8'
         if response.status_code != requests.codes.ok:
             raise RequestException('{}: {}'.format(response.status_code, response.text))
